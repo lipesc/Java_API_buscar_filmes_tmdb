5c514da3db37820088fd3fdf0102714284fc867c
lipesc <felipedrhouse@hotmail.com>
2025-02-03 10:34:13 -0300

    add allow cross origin para o frontend

--- src/main/java/com/lipesc/apitmdb/controller/MovieController.java
+++ src/main/java/com/lipesc/apitmdb/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.lipesc.apitmdb.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/movies")
+@CrossOrigin("/**")
 public class MovieController {
 
     private final MovieService movieService;
