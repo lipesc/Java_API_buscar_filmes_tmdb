157801e759d20f867b6d7f218a0d6762a2be5789
lipesc <felipedrhouse@hotmail.com>
2025-03-10 18:06:41 -0300

    fix controller cors

--- src/main/java/com/lipesc/apitmdb/controller/MovieController.java
+++ src/main/java/com/lipesc/apitmdb/controller/MovieController.java
@@ -1,6 +1,5 @@
 package com.lipesc.apitmdb.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +13,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/movies")
-@CrossOrigin("/**")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MovieController {
 
     private final MovieService movieService;
